1f9d2890a78e0d7a965b629d1dafa77171c83464
mpaulosky <60372079+mpaulosky@users.noreply.github.com>
2023-08-19 18:29:58 -0700

    Added AuthenticationStateProviderHelper

---
+++ src/BlogService.UI/Helpers/AuthenticationStateProviderHelpers.cs
@@ -0,0 +1,22 @@
+﻿// ============================================
+// Copyright (c) 2023. All rights reserved.
+// File Name :     AuthenticationStateProviderHelpers.cs
+// Company :       mpaulosky
+// Author :        Matthew Paulosky
+// Solution Name : BlogServiceApp
+// Project Name :  BlogService.UI
+// =============================================
+
+namespace BlogService.UI.Helpers;
+
+public static class AuthenticationStateProviderHelpers
+{
+	public static async Task<User> GetUserFromAuth(
+		this AuthenticationStateProvider provider,
+		IUserService userService)
+	{
+		var authState = await provider.GetAuthenticationStateAsync();
+		var objectId = authState.User.Claims.FirstOrDefault(c => c.Type.Contains("objectidentifier"))?.Value;
+		return await userService.GetByAuthIdAsync(objectId);
+	}
+}
