f9a253b6ee4b41e22fda882367bcc98b0e853f00
mpaulosky <60372079+mpaulosky@users.noreply.github.com>
2023-08-19 18:31:39 -0700

    Added new and removed unused shared components

---
+++ src/BlogService.UI/Shared/BlogPosts.razor.cs
@@ -0,0 +1,22 @@
+// ============================================
+// Copyright (c) 2023. All rights reserved.
+// File Name :     BlogPosts.razor.cs
+// Company :       mpaulosky
+// Author :        Matthew Paulosky
+// Solution Name : BlogServiceApp
+// Project Name :  BlogService.UI
+// =============================================
+
+namespace BlogService.UI.Shared;
+
+public partial class BlogPosts
+{
+	private List<BlogPost> Posts { get; set; } = new();
+
+	const string PlaceHolderImage = "https://via.placeholder.com/1060x180";
+
+	protected override async Task OnInitializedAsync()
+	{
+		Posts = await BlogService.GetAllAsync();
+	}
+}
---
+++ src/BlogService.UI/Shared/NavMenu.razor.cs
@@ -0,0 +1,15 @@
+// ============================================
+// Copyright (c) 2023. All rights reserved.
+// File Name :     NavMenu.razor.cs
+// Company :       mpaulosky
+// Author :        Matthew Paulosky
+// Solution Name : BlogServiceApp
+// Project Name :  BlogService.UI
+// =============================================
+
+namespace BlogService.UI.Shared;
+
+public partial class NavMenu
+{
+	private bool _collapseNavMenu = true;
+}
