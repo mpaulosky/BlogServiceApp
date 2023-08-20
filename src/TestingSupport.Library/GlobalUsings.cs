7a25018c7e4afe8a540962b30d545ed759fc5759
mpaulosky <60372079+mpaulosky@users.noreply.github.com>
2023-08-19 18:34:35 -0700

    Added Testing support and changes to the solution

---
+++ src/TestingSupport.Library/GlobalUsings.cs
@@ -0,0 +1,12 @@
+﻿// ============================================
+// Copyright (c) 2023. All rights reserved.
+// File Name :     GlobalUsings.cs
+// Company :       mpaulosky
+// Author :        Matthew Paulosky
+// Solution Name : BlogServiceApp
+// Project Name :  TestingSupport.Library
+// =============================================
+
+global using System.Diagnostics.CodeAnalysis;
+global using MongoDB.Driver;
+global using Moq;
